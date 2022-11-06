3f5ddc05612ea56587a20e96d26c46ce84c0897a
ghost-sys-coder <juniorbeast177@gmail.com>
2022-11-06 15:26:25 +0300

    Second Commit and we are now using Passport for authetication

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const e = require('express');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -13,6 +16,18 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+
+/**
+ * ! Setting up Passport just we initialize or Create our User Database
+ */
+app.use(session({
+    secret: "This is the database encryption where my head lies and my name is tamale plus i can not sleep.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 /**
  * ! BUILDING OUR USER DATABASE IN MONGODB AND MONGOOSE
@@ -28,16 +43,25 @@ const userSchema = mongoose.Schema({
 })
 
 /**
+ * ! Running Passport-local-mongoose for our Schema
+ */
+userSchema.plugin(passportLocalMongoose);
+
+/**
  * ! Creating an Encryption Key for the user Data
  */
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
 
 
 /**
  * ! Creating a Mongoose Model
  */
 const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res) => {
     res.render("home");
@@ -51,46 +75,59 @@ app.get('/register', (req, res) => {
     res.render("register");
 });
 
+app.get("/secrets", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect('/login');
+    }
+})
+
 /**
  * ! CAPTURING USER DATA FROM THEN REGISTER PAGE
  */
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
 
-    newUser.save(function (err) {
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
         if (err) {
             console.log(err);
+            res.redirect("/register");
         } else {
-            res.render("secrets");
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            })
         }
-    });
-
+    })
 });
+
+app.get("/logout", function (req, res) {
+    req.logout(function (err) {
+        if (err) throw err;
+        res.redirect("/");
+    });
+})
 
 /**
  * ! CHECKING IF THE USER EXISTS AND THEN LOGGING THEM IN -- LOGIN PAGE
  */
 app.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-    User.findOne({ email: username }, (err, foundUser) => {
+    req.login(user, function (err) {
         if (err) {
             console.log(err);
         } else {
-            if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
-            }
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets")
+            })
         }
-    });
+    })
 });
 
 
-app.listen(port, (res, req) => {
+app.listen(port, () => {
     console.log(`Server is running on ${port}....`)
 })
